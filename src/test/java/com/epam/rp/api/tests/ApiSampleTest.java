7c587b55d5c770c6539c2b43014f1657a0418d6d
disegmvm <dees3g@gmail.com>
2023-10-20 16:18:59 +0200

    Sample API test rename

--- src/test/java/com/epam/rp/api/tests/ApiSampleTest.java
+++ src/test/java/com/epam/rp/api/tests/ApiSampleTest.java
@@ -1,11 +1,11 @@
-package com.epam.rp.api;
+package com.epam.rp.api.tests;
 
 import com.epam.rp.utils.Logger;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Test;
 
 @Feature("SampleFeature")
-public class AllureSampleTest {
+public class ApiSampleTest {
     @Test
     public void testUserLogin() {
         Logger.logDebugMessage("ALLURE debug API test message");
