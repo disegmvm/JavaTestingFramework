698a497bf8863b5903b0bf38dd069a9bc10d4058
disegmvm <dees3g@gmail.com>
2023-10-26 22:25:50 +0200

    Parameterize API tests

--- src/test/java/com/epam/rp/runners/JUnit5TestRunner.java
+++ src/test/java/com/epam/rp/runners/JUnit5TestRunner.java
@@ -1,14 +1,11 @@
 package com.epam.rp.runners;
 
-import com.epam.rp.api.tests.ApiUserTest;
-import com.epam.rp.ui.tests.UiLoginTest;
+import com.epam.rp.api.tests.ApiDashboardTest;
+import com.epam.rp.ui.tests.UiDashboardTest;
 import org.junit.platform.suite.api.*;
 
-//@SelectClasses({UiLoginTest.class, ApiUserTest.class})
-@SelectPackages("com.epam.rp.ui.tests")
+@SelectClasses({ApiDashboardTest.class, UiDashboardTest.class})
 @Suite
 @SuiteDisplayName("A JUnit 5 Test Runner")
 public class JUnit5TestRunner {
 }
-
-
